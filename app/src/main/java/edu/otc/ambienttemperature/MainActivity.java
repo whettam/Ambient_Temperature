3b503402f549e20ac76f36930e57211b1ccc4649
whettam <mjballard88@gmail.com>
2024-02-26 09:47:27 -0600

    Added second text box and variable for TYPE_AMBIENT_TEMPERATURE

--- app/src/main/java/edu/otc/ambienttemperature/MainActivity.java
+++ app/src/main/java/edu/otc/ambienttemperature/MainActivity.java
@@ -2,13 +2,19 @@ package edu.otc.ambienttemperature;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int TYPE_AMBIENT_TEMPERATURE = 0;
+
+    int currentTemp = Sensor.TYPE_AMBIENT_TEMPERATURE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println(currentTemp);
     }
 }
